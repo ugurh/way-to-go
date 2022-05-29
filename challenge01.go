0ae9c6a358b1c24f960ef814c6c6137ed5a0366e
harun ugur <ugur.harun@yandex.com>
2022-05-30 00:18:47 +0300

    Temperature Conversion

---
+++ challenge01.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Temperature Conversion
+type Celsius float32
+type Fahrenheit float32
+
+func main() {
+
+	// Temperature in ˚F = (Temp in °C * 9/5) + 32
+
+	result := toFahrenheit(100)
+
+	fmt.Printf("%f ˚C is equal to %f ˚F", Celsius(100), result)
+}
+
+func toFahrenheit(t Celsius) Fahrenheit {
+	return Fahrenheit(t*1.8 + 32)
+}
