4a2e3689fc4578df09e019856ee5c7cbdecbae8d
harun ugur <ugur.harun@yandex.com>
2022-06-02 14:08:55 +0300

    Season of a Month

---
+++ Challenge02.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Season of a Month
+	fmt.Println(getSeason(10))
+}
+
+func getSeason(month int) string {
+
+	switch month {
+	case 12, 1, 2:
+		return "Winter"
+	case 3, 4, 5:
+		return "Spring"
+	case 6, 7, 8:
+		return "Summer"
+	case 9, 10, 11:
+		return "Autumn"
+	default:
+		return "Season unknown"
+	}
+}
